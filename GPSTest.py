f1674e6913ef9a16510bab9898c032c3c465a7dd
Phoenix Racing <phoenix@phoenixracing.olin.edu>
2013-03-08 10:29:35 -0500

    Parsing of GPS Data

--- GPSTest.py
+++ GPSTest.py
@@ -10,6 +10,15 @@ to_zone   = tz.tzlocal()
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
+# Convert decimal Lat/Long to Degrees/Minutes/Seconds format
+def decdeg2dms(dd):
+	positive = dd >= 0
+	dd = abs(dd)
+	mnt,sec = divmod(dd*3600,60)
+	deg,mnt = divmod(mnt,60)
+	deg = deg if positive else -deg
+	return deg,mnt,sec
+
 while True:
 	try:
 		report = session.next()
@@ -27,12 +36,24 @@ while True:
 				print 'time:      ' , localtime
 			if hasattr(report, 'lat'):
 				print 'latitude:  ' , report.lat
+				print '        :  ' , decdeg2dms(report.lat)
+				if hasattr(report, 'epy'):
+					print '   error:  ' , report.epy
 			if hasattr(report, 'lon'):
 				print 'longitude: ' , report.lon
+				print '         : ' , decdeg2dms(report.lon)
+				if hasattr(report, 'epx'):
+					print '    error: ' , report.epx
 			if hasattr(report, 'alt'):
 				print 'altitude:  ' , report.alt
+				if hasattr(report, 'epv'):
+					print '   error:  ' , report.epv
 			if hasattr(report, 'speed'):
 				print 'speed:     ' , report.speed
+				if hasattr(report, 'eps'):
+					print 'error:     ' , report.eps
+			if hasattr(report, 'track'):
+				print 'heading:   ' , report.track
 	except KeyError:
 		pass
 	except KeyboardInterrupt:
