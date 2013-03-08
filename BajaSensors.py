a1a638c7f96578d819b629faecdb783e78fca621
Patrick <patrick@patrick-ubuntu.(none)>
2013-03-07 23:10:42 -0500

    added functionality for tachometer

--- BajaSensors.py
+++ BajaSensors.py
@@ -6,14 +6,37 @@ class Tachometer(RpiGpioDevice):
 	def __init__(self, pin):
 		super(Tachometer, self).__init__({pin:True})
 		self.alive = True
-		self.lastEdgeTime = datetime.datetime.now()
-		self.updateTime = .01
+		self.lastRisingEdgeTime = datetime.datetime.now()
+		self.updatePeriod = .01
+		self.lastState = self.pins[0].get()
+		self.averagedRPM
+		self.numberOfMagnets
+		self.alpha
+
+	def get(self):
+		self.averagedRPM
 
 	def run(self):
 		while self.alive:
-			print 'I\'m alive'
-			time.sleep(self.updateTime)
+			now = datetime.datetime.now()
+			state = self.pins[0].get()
 
+			#an edge of the magnet
+        	if state is not self.lastState:
+        	    self.lastState = state
+	
+	        	#a rising edge of the magnet
+	        	if state is False:
+	        	    dt = max(1, (now - self.lastRisingEdgeTime).microseconds)/1000000.0
+	        	    rpm = 60.0 / self.numberOfMagnets / dt
+	        	    self.averagedRPM = self.averagedRPM*(1-self.alpha) + rpm*self.alpha
+	        	    self.lastRisingEdgeTime = now
+	    	
+	    	   	#catch the case when the input stops
+	    	    elif now - self.lastRisingEdgeTime > datetime.timedelta(seconds=0.25):
+	    	    	print 'too slow'
+        	    	self.averagedRPM = max(1e-4,(self.averagedRPM * 2) / 3)
+            		self.lastRisingEdgeTime = now
 
 	def stop(self):
 		self.alive = False
