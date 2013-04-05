b6dc491d32be860344fe9f1ced10c4399815f2c5
Patrick Varin <patrick.varin@students.olin.edu>
2013-04-04 22:52:36 -0400

    creating basic tornado app with websockets

--- pi_sockets/server.py
+++ pi_sockets/server.py
@@ -1,16 +1,38 @@
-import tornado
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
 import tornado.web
+import tornado.websocket
 
-class MainHandler(tornado.web.RequestHandler):
+from tornado.options import define, options
+define("port", default=8080, help="run on the given port", type=int)
+define("host", default="0.0.0.0")
 
+class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, World")
+        self.render('index.html')
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+	def open(self):
+		print 'new connection'
+		self.write_message("connected")
+	
+	def on_message(self, message):
+		print 'message received %s' % message
+		self.write_message('message received %s' % message)
+
+	def on_close(self):
+		print 'connection closed'
 
 if __name__=="__main__":
-	application.listen(8080)
+	tornado.options.parse_command_line()
+	app = tornado.web.Application(
+		handlers=[
+			(r"/", IndexHandler),
+			(r"/ws", WebSocketHandler)
+		]
+	)
+	http_server = tornado.httpserver.HTTPServer(app)
+	http_server.listen(options.port, address=options.host)
 	tornado.ioloop.IOLoop.instance().start()
 
